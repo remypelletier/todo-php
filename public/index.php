56181de790e8be3dcaf96e1f06aca6d86493f924
Rémy Pelletier <remypelletier2@gmail.com>
2024-04-11 17:24:05 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todolist</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <main class="todolist__container">
+        <div class="todolist todolist__wrapper">
+            <h1>To Do List <span><img src="/img/todolist-icon.png" alt="To Do List Icon" class="icon__todolist"></span></h1>
+            <form action="#">
+                <input type="text" name="newTask" id="inputNewTask">
+                <button type="submit">Add</button>
+            </form>
+            <ul class="todolist__items">
+                <li class="todolist__item">
+                    <p>Clean the bathroom</p>
+                    <form class="todolist__item__form-delete" action="#">
+                        <button type="submit">
+                            <img src="/img/trash-icon.png" alt="trash icon" class="icon__trash">
+                        </button>
+                    </form>
+                </li>
+                <li class="todolist__item">
+                    <p>Clean the bathroom</p>
+                    <form class="todolist__item__form-delete" action="#">
+                        <button type="submit">
+                            <img src="/img/trash-icon.png" alt="trash icon" class="icon__trash">
+                        </button>
+                    </form>
+                </li>
+                <li class="todolist__item">
+                    <p>Clean the bathroom</p>
+                    <form class="todolist__item__form-delete" action="#">
+                        <button type="submit">
+                            <img src="/img/trash-icon.png" alt="trash icon" class="icon__trash">
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </main>
+</body>
+</html>
